414e906f9d6e837c2b9d1ad3bfd861c0ff3fc789
LauraDico <lauradieguezconejo@gmail.com>
2021-02-13 17:33:01 +0100

    He añadido una lina preguntando que tal va.

--- deposito/src/cuentas/HolaMundo.java
+++ deposito/src/cuentas/HolaMundo.java
@@ -18,6 +18,7 @@ public class HolaMundo {
         // TODO code application logic here
     
         System.out.println("¡Hola Mundo!");
+        System.out.println("¿Qué tal va todo?");
     }
     
 }
